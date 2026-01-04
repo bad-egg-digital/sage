c72377a71bc68eb681dc1c01105a2a7988091f5e
Steve Ross <git.commits@mailbox.org>
2026-01-04 18:02:36 +0000

    add placeholder js to example block

--- resources/views/blocks/example/script.js
+++ resources/views/blocks/example/script.js
@@ -1,1 +1,2 @@
 // block.json's script, loaded in block editor and front end
+console.log('bad egg example block script.js loaded.');
--- resources/views/blocks/example/view.js
+++ resources/views/blocks/example/view.js
@@ -1,1 +1,2 @@
 // block.json's viewScript, applied on front end only
+console.log('bad egg example block view.js loaded.');
