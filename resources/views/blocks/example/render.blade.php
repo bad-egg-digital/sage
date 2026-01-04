afa130a3a51d9c726d1929230b8d44ead2a8d57e
Steve Ross <git.commits@mailbox.org>
2026-01-04 18:00:05 +0000

    add some placeholder markup to dynamic example block

--- resources/views/blocks/example/render.blade.php
+++ resources/views/blocks/example/render.blade.php
@@ -1,3 +1,7 @@
-<div class="block-badegg-example">
-  <h2>Bad Egg Example Block (Blade)</h2>
+<div {!! get_block_wrapper_attributes([
+  'class' => 'section bg-success knockout',
+]) !!}>
+  <div class="container wysiwyg align-center">
+    <h2>Bad Egg Example Block (Blade)</h2>
+  </div>
 </div>
