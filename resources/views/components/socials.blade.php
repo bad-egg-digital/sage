0d6597b64a1ccc91bd9d2de85b89e11ed10ee4a3
Steve Ross <git.commits@mailbox.org>
2026-02-10 16:43:48 +0000

    components.socials: fix variable typo

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -1,8 +1,8 @@
-@if(socials)
+@if($socials)
   <ul class="socials nolist">
     @foreach($socials as $social)
       <li>
-        <a href="{{ get_field('url', $social) }}" rel="noopener nofollow noreferrer" target="_blank">
+        <a href="{{ get_field('url', $social) }}" rel="noopener nofollow noreferrer me" target="_blank">
           <i class="fa-brands fa-{{ get_field('fontawesome_brands', $social) }}"></i>
           <span class="visually-hidden">{{ get_the_title($social) }}</span>
         </a>
