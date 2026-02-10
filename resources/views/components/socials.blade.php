7072e8bc8a4f7cd9c1b305112fd7dab0f118057b
Steve Ross <git.commits@mailbox.org>
2026-02-10 15:51:11 +0000

    conditional socials

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -1,10 +1,12 @@
-<ul class="socials nolist">
-  @foreach($socials as $social)
-    <li>
-      <a href="{{ get_field('url', $social) }}" rel="noopener nofollow noreferrer" target="_blank">
-        <i class="fa-brands fa-{{ get_field('fontawesome_brands', $social) }}"></i>
-        <span class="visually-hidden">{{ get_the_title($social) }}</span>
-      </a>
-    </li>
-  @endforeach
-</ul>
+@if(socials)
+  <ul class="socials nolist">
+    @foreach($socials as $social)
+      <li>
+        <a href="{{ get_field('url', $social) }}" rel="noopener nofollow noreferrer" target="_blank">
+          <i class="fa-brands fa-{{ get_field('fontawesome_brands', $social) }}"></i>
+          <span class="visually-hidden">{{ get_the_title($social) }}</span>
+        </a>
+      </li>
+    @endforeach
+  </ul>
+@endif
