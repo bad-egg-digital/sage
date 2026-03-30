426d3616f7f2d9d61f8a42bda7792c812e97b003
Steve Ross <git.commits@mailbox.org>
2026-03-30 13:44:30 +0100

    default post class to disable or customise

---
+++ app/PostTypes/Post.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\PostTypes;
+use App\View\Composers;
+
+class Post
+{
+    public function __construct()
+    {
+        /*
+         * Default Post Type Disable
+         */
+        add_action( 'register_post_post_type_args', [ $this, 'disable' ], 10, 2 );
+        add_action( 'init', [ $this, 'unregister_tag' ]);
+        add_action( 'init', [ $this, 'unregister_category' ]);
+
+        /*
+         * Default Post Type Customisations
+         */
+        // add_filter( 'post_type_labels_post', [$this, 'labels']);
+        // add_filter( 'register_post_post_type_args', [$this, 'args'], 10, 2 );
+        // add_filter( 'pre_post_link', [$this, 'permalink'], 10, 3);
+    }
+
+    function disable($args, $postType)
+    {
+        $args['public'] = false;
+
+        return $args;
+    }
+
+    function remove_default_post_type()
+    {
+        remove_menu_page('edit.php');
+    }
+
+    function remove_default_post_type_menu_bar($wp_admin_bar)
+    {
+        $wp_admin_bar->remove_node( 'new-post' );
+    }
+
+    function unregister_category()
+    {
+        unregister_taxonomy_for_object_type('category', 'post');
+    }
+
+    function unregister_tag()
+    {
+        unregister_taxonomy_for_object_type('post_tag', 'post');
+    }
+
+    public function labels($labels)
+    {
+        $labels->singular_name = __('Article', 'badegg');
+        $labels->name = __('Articles', 'badegg');
+        $labels->menu_name = __('News', 'badegg');
+
+        return $labels;
+    }
+
+    public function args($args, $postType)
+    {
+        $args['rewrite'] = ['slug' => $this->slug()];
+        // $args['menu_icon'] = 'dashicons-welcome-widgets-menus';
+
+        return $args;
+    }
+
+    public function permalink($permalink, $post, $leavename)
+    {
+        if (get_post_type($post) == 'post')
+            return $this->slug() . $permalink;
+        else
+            return $permalink;
+    }
+
+    public function slug()
+    {
+        $page_for_posts = get_option('page_for_posts');
+
+        if(!$page_for_posts) return;
+
+        $slug_for_posts = get_post_field('post_name', $page_for_posts);
+
+        return $slug_for_posts;
+    }
+
+}
